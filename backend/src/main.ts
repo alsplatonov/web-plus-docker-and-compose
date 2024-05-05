98dfcc9068445f0ce9aa0e5bb4ef16c937ca16de
alsplatonov <platon1560@gmail.com>
2024-05-06 01:12:48 +0300

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  // app.enableCors();
+  app.use(cors());
 
   const config = new DocumentBuilder()
     .setTitle('KUPIPODARIDAY BACKEND')
